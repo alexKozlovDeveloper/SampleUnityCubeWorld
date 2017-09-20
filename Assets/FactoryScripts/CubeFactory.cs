110523946e19fdd24b6adbb1b87f61612777be25
MINSK\Aliaksei_Kazlou <aliaksei_kazlou@epam.com>
2017-09-20 15:59:00 +0300

    Add project

---
+++ Assets/FactoryScripts/CubeFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeFactory : MonoBehaviour
+{
+    public GameObject DirtCubePrefab;
+    public GameObject GrassCubePrefab;
+    public GameObject RockCubePrefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
